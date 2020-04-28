2619d7af4817655fdbf802f19be8120816159732
Herzen Cortes <herzencc@icloud.com>
2020-04-28 09:24:37 -0400

    changing logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Axios from 'axios'
+import Breweries from './components/breweries'
+import Gallery from './components/gallery'
+import Header from './components/header'
+import {Switch, Route} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+let yelpUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search"
+let yelpKey = ``
+
+let pexelsUrl = "https://api.pexels.com/v1/search"
+let pexelsKey = ""
+
+
+class App extends Component {
+state = {
+  allBreweries : [],
+  allCities: [],
+  locationCity: 'Miami',
+  locationState: 'Florida',
+  imgSrc: [],
+  imgSearch: 'Cold beer'
+}  
+
+componentDidMount = () => {
+  
+
+  Axios.get(yelpUrl, {
+    headers: {
+      Authorization: yelpKey
+    },
+    params: {
+      // limit: 50,
+      categories: `bars`,
+      location: {
+        city: this.state.locationCity,
+        country: "US",
+        address2: "",
+        address3: "",
+        state: this.state.locationState,
+        address1: "",
+        zip_code: ""
+    }
+  
+  }
+
+  })
+  .then(res=> this.setState({allBreweries:res.data}))
+  .catch(err => console.log(err))
+
+  Axios.get(pexelsUrl, {
+    headers: {
+      Authorization: pexelsKey
+    }, 
+    params: {
+      query: this.state.imgSearch
+    }
+  })
+  .then(res2 => this.setState({imgSrc:res2.data}))
+  .catch(err2 => console.log(err2))
+
 }
 
+render(){
+  
+  return (
+    <div>
+      <Header/>
+      <Breweries
+        breweries = {this.state.allBreweries}
+      />
+      <Gallery
+        Gallery = {this.state.imgSrc}
+      />
+    </div>
+  );
+  }
+  }
 export default App;
