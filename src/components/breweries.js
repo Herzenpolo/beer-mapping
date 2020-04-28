d31cf0a616df83e97934edf75b41ae59b41731e0
Herzen Cortes <herzencc@icloud.com>
2020-04-27 20:33:58 -0400

    first react project commit

---
+++ src/components/breweries.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Breweries extends Component {
+   
+    
+    // showBreweries = () => {
+    //     return this.props.breweries.map(eachBrewery => console.log(eachBrewery))
+        
+    // }
+
+    render() {
+
+        console.log(this.props.breweries.businesses)
+        // this.showBreweries()
+        return (
+            <div>
+               
+            </div>
+        );
+    }
+}
+
+export default Breweries;
---
+++ src/components/gallery.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Gallery extends Component {
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                These are pictures
+            </div>
+        );
+    }
+}
+
+export default Gallery;
